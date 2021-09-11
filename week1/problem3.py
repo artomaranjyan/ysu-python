85bdbc68b284829243602210f075d0f88d74cd8a
Arto <arto.maranjyan@gmail.com>
2021-09-11 20:27:17 +0400

    problem3

---
+++ week1/problem3.py
@@ -0,0 +1,6 @@
+text, first_word, second_word = input(), input(), input()
+
+print(f"""The given text: {text}
+First word: {first_word}
+Second word: {second_word}
+Output string: {text.replace(first_word, second_word)}""")
